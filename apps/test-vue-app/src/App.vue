c718c6f364bd48ec6da39bfd8e0facef9a52ab52
Dev By Ray <6241164+devbyray@users.noreply.github.com>
2025-03-28 14:42:48 +0100

    feat: initialize test-vue-app with basic structure and components

---
+++ apps/test-vue-app/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import '@devbyray/vue-wc-components'
+</script>
+
+<template>
+  <header>
+    <!-- Button component -->
+    <vwc-button size="md" variant="primary">Click me</vwc-button>
+
+    <!-- Input component -->
+    <vwc-input placeholder="Enter your username"></vwc-input>
+
+    <!-- Icon component -->
+    <vwc-icon name="home" size="md" color="#FFD700"></vwc-icon>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  width: 100%;
+}
+</style>
